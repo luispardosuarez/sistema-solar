fe964ae18f8d5249ca778882b815cdbcb767d3ae
Luis Pardo <luispardosuarez@gmail.com>
2024-03-08 10:23:04 +0100

    added Planeta.java code

--- src/main/java/dev/luispardo/sistemasolar/Planeta.java
+++ src/main/java/dev/luispardo/sistemasolar/Planeta.java
@@ -1,5 +1,49 @@
 package dev.luispardo.sistemasolar;
 
 public class Planeta {
-  
+  private String nombre = null;
+  private int cantidadSatelites = 0;
+  private double masa = 0;
+  private double volumen = 0;
+  private int diametro = 0;
+  private int distanciaMediaAlSol = 0;
+  private TipoPlaneta tipo;
+  private boolean observableSimpleVista = false;
+  private double periodoOrbital = 0; // Años
+  private double periodoRotacion = 0; // Días
+
+  // Constructor
+  public Planeta(String nombre, int cantidadSatelites, double masa, double volumen, int diametro,
+                 int distanciaMediaAlSol, TipoPlaneta tipo, boolean observableSimpleVista, double periodoOrbital, double periodoRotacion) {
+      this.nombre = nombre;
+      this.cantidadSatelites = cantidadSatelites;
+      this.masa = masa;
+      this.volumen = volumen;
+      this.diametro = diametro;
+      this.distanciaMediaAlSol = distanciaMediaAlSol;
+      this.tipo = tipo;
+      this.observableSimpleVista = observableSimpleVista;
+      this.periodoOrbital = periodoOrbital;
+      this.periodoRotacion = periodoRotacion;
+  }
+
+  // Método para calcular densidad
+  public double calcularDensidad() {
+      return masa / volumen;
+  }
+
+  // Determinar si es un planeta exterior
+  public boolean esExterior() {
+      double distanciaEnAU = distanciaMediaAlSol / 149597870.0;
+      return distanciaEnAU > 3.4;
+  }
+
+  // Imprimir información del planeta
+  public void imprimirInfo() {
+      System.out.println("Nombre: " + nombre);
+      System.out.println("Densidad: " + calcularDensidad() + " kg/m^3");
+      System.out.println("Es exterior: " + esExterior());
+      System.out.println("Periodo Orbital: " + periodoOrbital + " años");
+      System.out.println("Periodo de Rotación: " + periodoRotacion + " días");
+  }
 }
